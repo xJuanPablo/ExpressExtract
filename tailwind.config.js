65750024d6fde29546cc345c92138845df397d9c
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-10-15 16:57:21 -0600

    Added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    //TODO Put custom font family in extend
+    //TODO: Import fonts into index.html
+    extend: {},
+  },
+  plugins: [],
+}
