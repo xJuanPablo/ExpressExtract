a12a9f44573c52f5fe2d1a252cfdc825ea815c12
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-10-16 15:37:45 -0600

    Set up API with ReduxJS

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,16 +2,31 @@ import React, {useEffect, useState} from 'react'
 import {FiLink2} from 'react-icons/fi'
 import {BiSearchAlt} from 'react-icons/bi'
 
+
 function Demo() {
+
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  const submit = async (e) => {
+    alert('OHHHHH')
+  }
+
+
+
   return (
     <section className='mt-16 w-full max-w-xl'>
       <div className='flex flex-col w-full gap-2'>
-        <form action="" className='relative flex justify-center items-center' onSubmit={() => {}}>
+        <form action="" className='relative flex justify-center items-center' onSubmit={submit}>
           <FiLink2 className='absolute left-0 my-2 ml-3 w-5'/>
           <input type="url" 
           placeholder="Enter a URL"
-          value=''
-          onChange={() => {}}
+          value={article.url}
+          onChange={(e) => setArticle({ ...article, url: e.target.value
+
+          })}
           required
           className='url_input peer'
           />
@@ -22,7 +37,7 @@ function Demo() {
             <BiSearchAlt/>
           </button>
         </form>
-        
+
       </div>
 
     </section>
